071b3d722c6693d9489d560ea4137aabe8ad2a90
tusky <isaac@dw-perspective.org.uk>
2025-03-13 17:23:10 +0000

    practise read in and write files in Java

---
+++ inclass/march/writingtoFiles.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class writingtoFiles{
+    public static void main(String[] args) {
+        try{
+            PrintWriter out = new PrintWriter(new FileWriter("out.txt"));
+            String name = "Bob Dylan";
+            double score = 999.4;
+            out.print(name);
+            out.print(" ");
+            out.println(score);
+            out.close();
+        } catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
