8688b88182dc0e040692f714c50a59d8f36360c5
tusky <isaac@dw-perspective.org.uk>
2025-03-20 18:03:43 +0000

    Added driver, added questions to storage, added functionality to Quiz

--- labs/week08/Quiz.java
+++ labs/week08/Quiz.java
@@ -1,31 +1,47 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 import java.io.*;
-
+import java.lang.reflect.Array;
+import java.util.random.*;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 public class Quiz {
     private String user;
     private int score;
     private int attempted;
     private ArrayList<Question> questions;
     private boolean continueQuiz;
-    private String questionsFileLoc;
+    private String questionDir;
 
-    public Quiz(String user) {
-        this.user = user;
+    public Quiz(String name, String dir) {
+        this.user = name;
+        this.questionDir = dir;
         this.score = 0;
+        this.questions = new ArrayList<Question>();
+
+        this.getQuestions();
+        this.beginQuiz();
+
     }
 
     public void setQuestionsLoc(String dir){
-        this.questionsFileLoc = dir;
+        this.questionDir = dir;
     }
 
     public void getQuestions(){
         try{
-            File questionFile = new File("this.questionsFileLoc");
+            File questionFile = new File(this.questionDir);
             Scanner scanner = new Scanner(questionFile);
-            
+            ArrayList<ArrayList<String>> questions = new ArrayList<ArrayList<String>>();
             while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                ArrayList<String> temp = new ArrayList<String>(Arrays.asList(line.split(", ")));
 
+                ArrayList<String> tempOptions = new ArrayList<String>(Arrays.asList(temp.get(1), temp.get(2), temp.get(3), temp.get(4)));
+                Question question = new Question(temp.getFirst(), temp.getLast(), tempOptions);
+                this.questions.add(question);
             }
             scanner.close();
         } catch(FileNotFoundException ex){
@@ -34,13 +50,20 @@ public class Quiz {
        
     }
 
-    public void holdQuiz() {
-        // access file
-        // read in questions
+    public void beginQuiz() {
+        this.continueQuiz = true;
+        Scanner input = new Scanner(System.in);
 
-        while (continueQuiz){
+        while(this.continueQuiz){
+            Question iterQuestion = this.questions.get(0);
+            System.out.println(iterQuestion.getQuestion());
+            System.out.println("Your options are:" + iterQuestion.getOptions());
 
+            String response = String.valueOf(input.nextLine());
+            System.out.println(iterQuestion.validateAnswer(response));
+            this.continueQuiz=false;
         }
+        input.close();
     }
     public void endQuiz() {}
 }
