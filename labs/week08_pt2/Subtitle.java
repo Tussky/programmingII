62053d5b22a8a974676d610294f5101ae1a56306
tusky <isaac@dw-perspective.org.uk>
2025-03-25 16:53:23 +0000

    created subtitle interface and time interface

---
+++ labs/week08_pt2/Subtitle.java
@@ -0,0 +1,8 @@
+public interface Subtitle {
+    Time getStarTime();
+    Time getendTime();
+    String getText();
+    void setStartTime(Time startTime);
+    void setEndTime(Time endTime);
+    void setText(String Text)
+}
